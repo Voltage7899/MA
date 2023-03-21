6ff5a396eb96888496f1a5411aa4351a5a1f214c
Ser <ivanov.kir2001@gmail.com>
2023-03-21 20:37:20 +0300

    500

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,13 +10,13 @@ class TestDociments():
 
     def test_empty_get_Docs(self):
         response = requests.get("http://localhost:8000/doc/docs")
-        assert response.status_code == 200
+        assert response.status_code == 500
 
 
     def test_post_Doc(self):
         body = {"title": "shittitle", "body": "shitBody"}
         response = requests.post("http://localhost:8000/doc/docs",json=body)
-        assert response.status_code == 500
+        assert response.status_code == 405
 
 
 
